19ad8616b4f0b051db4311fc9445e8fbb96de08e
marcosicp <marcosicp2012@gmail.com>
2021-06-02 18:42:55 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.859f45b43ffd18aa7278d6ae432e276b.js"
+  "/portfolio/precache-manifest.8b3412a0709609b5e17588808d5adf1a.js"
 );
 
 self.addEventListener('message', (event) => {
