e22805d864152e4202144e389dfeaafcf90aa1b5
marcosicp <marcosicp2012@gmail.com>
2021-06-02 06:00:45 -0300

    init portfolio

---
+++ src/components/about.jsx
@@ -0,0 +1,306 @@
+import React from "react";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        {
+          id: "HTML5_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "HTML5",
+          porcentage: "80%",
+          value: "80",
+        },
+        {
+          id: "CSS3_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "CSS3",
+          porcentage: "75%",
+          value: "75",
+        },
+        {
+          id: "JavaScript_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "JavaScript",
+          porcentage: "90%",
+          value: "90",
+        },
+        {
+          id: "Angular_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "Angular",
+          porcentage: "90%",
+          value: "90",
+        },
+        {
+          id: "AngularJS_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "AngularJS",
+          porcentage: "90%",
+          value: "90",
+        },
+      ],
+      skillsMobile: [
+        {
+          id: "Flutter_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "Flutter",
+          porcentage: "80%",
+          value: "80",
+        },
+        {
+          id: "Xamarin_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "Xamarin",
+          porcentage: "75%",
+          value: "75",
+        },
+        {
+          id: "AndroidNative(Java)_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "AndroidNative(Java)",
+          porcentage: "90%",
+          value: "90",
+        },
+      ],
+      skillsDB: [
+        {
+          id: "MySQL_skill",
+          content: "MySQL",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "80%",
+          value: "80",
+        },
+        {
+          id: "CosmosDB_skill",
+          content: "CosmosDB",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "75%",
+          value: "75",
+        },
+        {
+          id: "MongoDB_skill",
+          content: "MongoDB",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "85%",
+          value: "85",
+        },
+        {
+          id: "SQL_skill",
+          content: "SQL",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "80%",
+          value: "80",
+        },
+        {
+          id: "SQLite_skill",
+          content: "SQLite",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "80%",
+          value: "80",
+        },
+      ],
+      skillsBackEnd: [
+        {
+          id: "NodeJS_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "Node",
+          porcentage: "90%",
+          value: "90",
+        },
+        {
+          id: "C#_skill",
+          content: "C#",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "70%",
+          value: "70",
+        },
+        {
+          id: "FB-CloudFunctions_skill",
+          content: "Firebase Functions",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "70%",
+          value: "70",
+        },
+        {
+          id: "AZ-CloudFunctions_skill",
+          content: "Azure Functions",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "70%",
+          value: "70",
+        },
+      ],
+      skillsCloud: [
+        {
+          id: "Firebase_skill",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          content: "Firebase",
+          porcentage: "90%",
+          value: "90",
+        },
+        {
+          id: "Azure_skill",
+          content: "C#",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "70%",
+          value: "70",
+        },
+        {
+          id: "GitHub_skill",
+          content: "GitHub",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "80%",
+          value: "80",
+        },
+        {
+          id: "GitLab_skill",
+          content: "GitLab",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "75%",
+          value: "75",
+        },
+        {
+          id: "BitBucket_skill",
+          content: "BitBucket",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "75%",
+          value: "75",
+        },
+        {
+          id: "CodeMagic_skill",
+          content: "CodeMagic",
+          image: "https://img.icons8.com/wired/64/000000/javascript-logo.png",
+          porcentage: "75%",
+          value: "75",
+        },
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "Software developer with full-stack profile orientated to .Net technologies for the back-end, Angular on the front-end and finally Flutter when build something for mobile. Have experience working with different kind of projects from scratch and giving support. Technical capacities and good skills to work with big and small teams always following best practices. Knowledge with deployments, managing continuous integration and continuous deployments. Always trying to improve as a person and as a software developer.",
+        },
+        // {
+        //   id: "second-p-about",
+        //   content:
+        //     "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.",
+        // },
+        // {
+        //   id: "third-p-about",
+        //   content:
+        //     "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.",
+        // },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <h4>FrontEnd</h4>
+                    <div className="skill-mf">
+                      {this.state.skills.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <div className="chip">{skill.content}</div>{" "}
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+
+                    <h4>BackEnd</h4>
+                    <div className="skill-mf">
+                      {this.state.skillsBackEnd.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <div className="chip">{skill.content}</div>{" "}
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                    <h4>Mobile</h4>
+                    <div className="skill-mf">
+                      {this.state.skillsMobile.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <div className="chip">
+                              {skill.content}
+                            </div>{" "}
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                    <h4>Databases</h4>
+                    <div className="skill-mf">
+                      
+                      {this.state.skillsDB.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <div className="chip">
+                              {skill.content}
+                            </div>{" "}
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                    <h4>Cloud</h4>
+                    <div className="skill-mf">
+                      {this.state.skillsCloud.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <div className="chip">
+                              {skill.content}
+                            </div>{" "}
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">Techs</h5>
+                      </div>
+                      {this.state.about_me.map((content) => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/contact.jsx
@@ -0,0 +1,212 @@
+import React from "react";
+import imageOverlay from "../img/earth.jpg";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <section
+        className="paralax-mf footer-paralax bg-image sect-mt4 route"
+        style={{ backgroundImage: "url(" + imageOverlay + ")" }}
+      >
+        <div className="overlay-mf"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="contact-mf">
+                <div id="contact" className="box-shadow-full">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="title-box-2">
+                        <h5 className="title-left">Send A Message</h5>
+                      </div>
+                      <div>
+                        <form
+                          action="https://formspree.io/xdoeonlo"
+                          method="POST"
+                          className="contactForm"
+                        >
+                          <div id="sendmessage">
+                            Your message has been sent. Thank you!
+                          </div>
+                          <div id="errormessage"></div>
+                          <div className="row">
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  name="name"
+                                  className="form-control"
+                                  id="name"
+                                  placeholder="Your Name"
+                                  data-rule="minlen:4"
+                                  data-msg="Please enter at least 4 chars"
+                                />
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="email"
+                                  className="form-control"
+                                  name="email"
+                                  id="email"
+                                  placeholder="Your Email"
+                                  data-rule="email"
+                                  data-msg="Please enter a valid email"
+                                />
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  name="subject"
+                                  id="subject"
+                                  placeholder="Subject"
+                                  data-rule="minlen:4"
+                                  data-msg="Please enter at least 8 chars of subject"
+                                />
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                              <div className="form-group">
+                                <textarea
+                                  className="form-control"
+                                  name="message"
+                                  rows="5"
+                                  data-rule="required"
+                                  data-msg="Please write something for us"
+                                  placeholder="Message"
+                                ></textarea>
+                                <div className="validation"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-12">
+                              <button
+                                type="submit"
+                                className="button button-a button-big button-rouded"
+                              >
+                                Send Message
+                              </button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="title-box-2 pt-4 pt-md-0">
+                        <h5 className="title-left">Get in Touch</h5>
+                      </div>
+                      <div className="more-info">
+                        <p className="lead">
+                          Whether you want to get in touch, talk about a project
+                          collaboration, or just say hi, I'd love to hear from
+                          you.
+                          <br />
+                          Simply fill the from and send me an email.
+                        </p>
+                        {/* <!-- <ul class="list-ico">
+                                <li><span class="ion-ios-location"></span> 329 WASHINGTON ST BOSTON, MA 02108</li>
+                                <li><span class="ion-ios-telephone"></span> (617) 557-0089</li>
+                                <li><span class="ion-email"></span> contact@example.com</li>
+                                </ul> --> */}
+                      </div>
+                      <div className="socials">
+                        <ul>
+                          <li>
+                            <a
+                              href="https://www.facebook.com/marcos.casor"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-social-facebook"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://github.com/marcosicp"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-social-github"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://stackoverflow.com/users/1518978"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                            <span className="ico-circle">
+                            <img align="center" className="imgSO"
+                                src="https://camo.githubusercontent.com/bd44ccfd9dad8d42fb19cacdac1ce3d2da666127da16876455a661988cf65f5d/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f6e706d2f73696d706c652d69636f6e7340332e302e312f69636f6e732f737461636b6f766572666c6f772e737667" alt="stackoverflow" 
+                                height="25" width="34" data-canonical-src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/stackoverflow.svg" />
+                            </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://www.linkedin.com/in/marcos-casor-pressacco-086ab321/?locale=en_US"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-social-linkedin"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="https://www.instagram.com/marcos.casor"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-social-instagram"></i>
+                              </span>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="mailto: marcos.casor@gmail.com"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <span className="ico-circle">
+                                <i className="ion-email"></i>
+                              </span>
+                            </a>
+                          </li>
+                         
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="copyright-box"></div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </section>
+    );
+  }
+}
+
+export default Contact;
